f4bec50f9f39717285692b84b128169446d42949
Manuel <mlagunas94@gmail.com>
2016-02-11 22:08:44 +0100

    Turns

--- HashCode/src/TurnCalculation/ScoreArray.java
+++ HashCode/src/TurnCalculation/ScoreArray.java
@@ -47,25 +47,22 @@ public class ScoreArray {
 		return Math.round(turn);
 	}
 
-	public static HashMap<Order, Long> getTurns(Drone dron,
+	public static Order getTurns(Drone dron,
 			ArrayList<Order> orders, ArrayList<Warehouse> whs) {
-
-		HashMap<Order, Long> scores = new HashMap<Order, Long>();
 		long best = Long.MAX_VALUE;
 		Order bestO = null;
 		for (Order o : orders) {
 			for (Warehouse w : whs) {
 				if (hasProduts(w, o)) {
 					long valor = traceTurn(dron, o, w);
-					if (valor < best){
+					if (valor != -1 && valor < best){
 						bestO = o;
 						best = valor;
 					}
 				}
 			}
-			scores.put(o, best);
 		}
-		return scores;
+		return bestO;
 	}
 
 }
